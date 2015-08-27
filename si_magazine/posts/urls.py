fd36b16fe5c5b4968ae342db11f57d6091701fe2
Pedro Henrique <pedrofagundesb@gmail.com>
2015-08-27 11:19:35 -0300

    Making some changes on template color; Done filtering posts by Tags or Categories

--- si_magazine/posts/urls.py
+++ si_magazine/posts/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from .views import PostDetailView
+from .views import PostDetailView, PostsCategoryList, PostsTagList
 
 urlpatterns = [
+	url(r'^category/([\w-]+)/$', PostsCategoryList.as_view(), name='posts_category_list'),
+	url(r'^tag/([\w-]+)/$', PostsTagList.as_view(), name='posts_tag_list'),
 	url(r'^(?P<slug>[^\.]+)', PostDetailView.as_view(), name='post_detail')
 ]
--- si_magazine/posts/views.py
+++ si_magazine/posts/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
-from .models import Posts
+from .models import Posts, Categories, Tags
 
 class PostDetailView(DetailView):
 	model               = Posts
@@ -11,10 +11,33 @@ class PostDetailView(DetailView):
 
 	def get_context_data(self, **kwargs):
 	    context = super(PostDetailView, self).get_context_data(**kwargs)
+	    context['categories'] = Categories.objects.all()
 	    return context
 
-	def get_next(self):
-	    next = Post.objects.filter(id__gt=self.id)
-	    if next:
-	      return next[0]
-	    return False
+class PostsCategoryList(ListView):
+	template_name = 'posts/posts_category_list.html'
+	context_object_name = 'posts'
+
+	def get_queryset(self):
+		self.category = get_object_or_404(Categories, name=self.args[0])
+		return Posts.objects.filter(category=self.category)
+
+	def get_context_data(self, **kwargs):
+	    context = super(PostsCategoryList, self).get_context_data(**kwargs)
+	    self.category = get_object_or_404(Categories, name=self.args[0])
+	    context['category'] = self.category
+	    return context
+
+class PostsTagList(ListView):
+	template_name = 'posts/posts_tag_list.html'
+	context_object_name = 'posts'
+
+	def get_queryset(self):
+		self.tag = get_object_or_404(Tags, name=self.args[0])
+		return Posts.objects.filter(tags__name__contains=self.tag)
+
+	def get_context_data(self, **kwargs):
+	    context = super(PostsTagList, self).get_context_data(**kwargs)
+	    self.tag = get_object_or_404(Tags, name=self.args[0])
+	    context['tag'] = self.tag
+	    return context
